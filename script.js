8766552fbe308c4bc19a6afef5cf3bf21d9c96c1
Mohamed Riyas <47690769+Riyas10@users.noreply.github.com>
2023-09-03 18:45:36 +0400

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
             try{
                 const response = await fetch("https://api.quotable.io/random");
                 const data = await response.json();
-                return data.content + "-" + data.author;
+                return data.content + "---" + data.author;
             } catch(error) {
                 console.log("Error : " + error);
                 return "Failed to fetch a quote. Please try again later."
